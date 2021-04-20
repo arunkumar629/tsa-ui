8b551c492b0d9a11c31cf07533b741690a8344e4
arun <arunkumar629@gmail.com>
2021-04-20 12:07:25 +0530

    4_graph_added

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -9,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 filename:any;
-filename1:any;
+filename_sma:any;
+filename_wma:any;
+filename_ewma:any;
 baseUrl:string='http://localhost:5000/static/';
 img:string="_basic.jpg";
-img1:string="_sma.jpg"
+img_sma:string="_sma.jpg";
+img_wma:string="_wma.jpg"
+img_ewma:string="_ewma.jpg"
   constructor(private router: Router, private fileUploadService: FileUploadService) { }
 
   ngOnInit(): void {
@@ -21,7 +25,9 @@ this.filename=localStorage.getItem('file');
 this.filename=this.filename.slice(0,-4)
 this.baseUrl=this.baseUrl.concat(this.filename.toString());
 this.filename=this.baseUrl.concat(this.img.toString());
-this.filename1=this.baseUrl.concat(this.img1.toString());
+this.filename_sma=this.baseUrl.concat(this.img_sma.toString());
+this.filename_wma=this.baseUrl.concat(this.img_wma.toString());
+this.filename_ewma=this.baseUrl.concat(this.img_ewma.toString());
 
   }
 
