4e8ba39d25acf2603203cd1321dfd6f36142a51a
arun <arunkumar629@gmail.com>
2021-05-03 10:06:19 +0530

    anomaly_updated

--- src/app/analomy/analomy.component.ts
+++ src/app/analomy/analomy.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-analomy',
   templateUrl: './analomy.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalomyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+let name=localStorage.getItem('file');
+    this.http.get('//localhost:5000/anomaly/'+name).subscribe((data: any)=>{
+      
+//alert(data);  
+    });
   }
 
 }
